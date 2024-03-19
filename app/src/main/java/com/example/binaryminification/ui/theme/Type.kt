db76c3b19ef858e13d8ec5fbf47411d41614d97c
Minger Ekaterina <minger.ekaterina@gmail.com>
2024-03-19 11:39:21 +0600

    Create sketch of main screen

--- app/src/main/java/com/example/binaryminification/ui/theme/Type.kt
+++ app/src/main/java/com/example/binaryminification/ui/theme/Type.kt
@@ -3,6 +3,7 @@ package com.example.binaryminification.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -14,21 +15,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Italic,
+        fontSize = 32.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
--- app/src/main/java/com/example/binaryminification/ui/theme/screens/MainScreen.kt
+++ app/src/main/java/com/example/binaryminification/ui/theme/screens/MainScreen.kt
@@ -1,2 +1,51 @@
 package com.example.binaryminification.ui.theme.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.binaryminification.R
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .paint(
+            painterResource(id = R.drawable.main_background),
+            contentScale = ContentScale.Crop,
+        )
+    )
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                modifier = Modifier.size(152.dp),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(id = R.string.logo)
+            )
+
+            Text(
+                text = stringResource(id = R.string.logic_calc),
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.White,
+            )
+        }
+    }
